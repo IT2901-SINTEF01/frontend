ab29a91403047fd8884ce6a2f06f876e4b0934b1
Emil Ommundsen <emilom@stud.ntnu.no>
2021-02-24 15:27:53 +0100

    Make placeholder for dashboard

--- src/components/molecules/DashboardItem.tsx
+++ src/components/molecules/DashboardItem.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
+import { Card, Pane, Heading } from 'evergreen-ui';
 
-const DashboardItemStyle = {
-    height: '100%',
-    width: '100%',
-    backgroundColor: '#fff',
-};
-
-type DashboardItemProps = {
-    label: string;
-};
-
-const DashboardItem: React.FC<DashboardItemProps> = ({ label }) => {
-    return <div style={DashboardItemStyle}>{label}</div>;
+const DashboardItem: React.FC = () => {
+    console.log('title');
+    return <Card width={'100%'} height={'100%'} border="default" elevation={1}></Card>;
 };
 
 export default DashboardItem;
