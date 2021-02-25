450ee1156272d9341bc14c7c9973922775c17c14
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-02-25 14:54:48 +0100

    Remove maximisation feature
    Temporarily disabled. Left to frontend gang to figure out how to solve this without a lot of unncessary renders.

--- src/components/organisms/Dashboard.tsx
+++ src/components/organisms/Dashboard.tsx
@@ -1,82 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useMemo } from 'react';
 import ReactGridLayout from 'react-grid-layout';
 import { ParentSize } from '@visx/responsive';
 
-import { Pane, MinimizeIcon, MaximizeIcon, IconButton } from 'evergreen-ui';
+import { Pane } from 'evergreen-ui';
 
 import DashboardItem from '../molecules/DashboardItem';
-import { DashboardItemSize, DashboardItemInfo, DashboardProps } from '../../dataTypes/Dashboard/DashboardTypes';
+import { DashboardProps } from '../../dataTypes/Dashboard/DashboardTypes';
 
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
-
-type Layout = {
-    i: string;
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-};
+import { resolvePositions } from '../../utils/dashboardList';
 
 const Dashboard: React.FC<DashboardProps> = ({ items }) => {
-    const [layout, setLayout] = useState<Layout[]>();
-
-    const checkIfItemFits = (x: number, size: DashboardItemSize) => {
-        if (size === DashboardItemSize.SMALL && x < 5) return true;
-        if (size === DashboardItemSize.MEDIUM && x < 4) return true;
-        if (size === DashboardItemSize.LARGE && x < 3) return true;
-        return false;
-    };
-
-    const getItemWidth = (dashboardItemSize: DashboardItemSize) => {
-        return dashboardItemSize.valueOf();
-    };
-
-    useEffect(() => {
-        //Creates layout first time
-        const gridLayout: Layout[] = [];
-        let x = 0;
-        let y = 0;
-        items.forEach((item: DashboardItemInfo, index: number) => {
-            if (index === 0) {
-                gridLayout.push({
-                    i: index.toString(),
-                    x,
-                    y,
-                    w: getItemWidth(item.size),
-                    h: 1,
-                });
-                x += getItemWidth(item.size);
-            } else {
-                if (checkIfItemFits(x, item.size)) {
-                    gridLayout.push({
-                        i: index.toString(),
-                        x,
-                        y,
-                        w: getItemWidth(item.size),
-                        h: 1,
-                    });
-                    x += getItemWidth(item.size);
-                } else {
-                    x = 0;
-                    y++;
-                    gridLayout.push({
-                        i: index.toString(),
-                        x,
-                        y,
-                        w: getItemWidth(item.size),
-                        h: 1,
-                    });
-                    x += getItemWidth(item.size);
-                }
-            }
-        });
-        setLayout(gridLayout);
-    }, [items]);
+    const layout = useMemo(() => resolvePositions(items), [items]);
 
     return (
-        <Pane height={'100%'} width={'80%'} marginLeft={'10%'}>
-            {console.log('layout change')}
+        <Pane height="100%" width="80%" marginLeft="10%">
             <ParentSize debounceTime={500}>
                 {({ width: visWidth }) => (
                     <ReactGridLayout
@@ -89,46 +28,13 @@ const Dashboard: React.FC<DashboardProps> = ({ items }) => {
                         useCSSTransforms={true}
                         isResizable={false}
                     >
-                        {layout &&
-                            layout.map((item) => {
-                                return (
-                                    <div key={item.i}>
-                                        <DashboardItem />
-                                        {item.w === 4 && (
-                                            <IconButton
-                                                icon={MaximizeIcon}
-                                                appearance="minimal"
-                                                position={'absolute'}
-                                                color="muted"
-                                                top={5}
-                                                right={5}
-                                                onClick={() => {
-                                                    const newLayout = layout.map((item) => ({ ...item }));
-                                                    newLayout[+item.i].h = 2;
-                                                    newLayout[+item.i].w = 6;
-                                                    setLayout(newLayout);
-                                                }}
-                                            />
-                                        )}
-                                        {item.w === 6 && (
-                                            <IconButton
-                                                icon={MinimizeIcon}
-                                                appearance="minimal"
-                                                position={'absolute'}
-                                                color="muted"
-                                                top={5}
-                                                right={5}
-                                                onClick={() => {
-                                                    const newLayout = layout.map((item) => ({ ...item }));
-                                                    newLayout[+item.i].h = 1;
-                                                    newLayout[+item.i].w = 4;
-                                                    setLayout(newLayout);
-                                                }}
-                                            />
-                                        )}
-                                    </div>
-                                );
-                            })}
+                        {layout.map((item) => {
+                            return (
+                                <div key={item.i}>
+                                    <DashboardItem />
+                                </div>
+                            );
+                        })}
                     </ReactGridLayout>
                 )}
             </ParentSize>
