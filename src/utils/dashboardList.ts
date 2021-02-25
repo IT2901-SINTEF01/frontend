55b562cce378620dba85fbcb8699bdb3b351d88b
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-02-25 14:53:05 +0100

    Helper to resolve grid positions with a reduce

--- src/utils/dashboardList.ts
+++ src/utils/dashboardList.ts
@@ -7,3 +7,30 @@ export const generateRandomList = (size: number): DashboardItemInfo[] =>
         ],
         withText: false,
     }));
+
+/**
+ * Used to track the layout of a single item.
+ * [i]ndex
+ * [x]pos
+ * [y]pos
+ * [w]idth
+ * [h]eight
+ */
+export type Layout = Record<'x' | 'y' | 'w' | 'h', number> & { i: string };
+
+const validItemPosition = (x: number, size: DashboardItemSize) => {
+    return 6 - x - size.valueOf() >= 0;
+};
+
+export const resolvePositions = (items: Array<DashboardItemInfo>): Array<Layout> =>
+    items.reduce((prev, curr, i) => {
+        const is = String(i);
+        if (prev.length === 0) return [{ i: is, x: 0, w: curr.size.valueOf(), y: 0, h: 1 }];
+        const last = prev.slice(-1)[0]; // discard every element but the last
+        return [
+            ...prev,
+            validItemPosition(last.x + last.w, curr.size)
+                ? { i: is, x: last.x + last.w, y: last.y, w: curr.size.valueOf(), h: 1 }
+                : { i: is, x: 0, w: curr.size.valueOf(), y: last.y + 1, h: 1 },
+        ];
+    }, [] as Array<Layout>);
