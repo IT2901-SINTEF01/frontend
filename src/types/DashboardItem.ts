d6681718f34f1c42ae3bbc6af806dbadca8fb891
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-02-26 14:12:29 +0100

    Add controls to storybook

--- src/types/DashboardItem.ts
+++ src/types/DashboardItem.ts
@@ -1,11 +1,11 @@
-import { ReactNode } from 'react';
-
-export type DashboardItemLargeType = {
+export type DashboardItemSmall = {
     title: string;
     titleSize: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
     width: number | string;
     height: number | string;
+};
+
+export type DashboardItemLarge = DashboardItemSmall & {
     paragraph: string;
     textSize: 300 | 400 | 500;
-    children: ReactNode;
 };
