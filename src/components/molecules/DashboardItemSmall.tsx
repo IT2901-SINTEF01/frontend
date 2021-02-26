d8c4fb98fc2f05c62d05a4bc1bdbe98e207ac28c
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-02-26 14:13:11 +0100

    Change type of small item

--- src/components/molecules/DashboardItemSmall.tsx
+++ src/components/molecules/DashboardItemSmall.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { DashboardItemSmallType } from '../../dataTypes/DashboardItems/DashboardItemSmallType';
+import { DashboardItemSmall as DashboardItemSmallT } from '../../types/DashboardItem';
 import { Card, Pane, Heading } from 'evergreen-ui';
 
-const DashboardItemSmall: React.FC<DashboardItemSmallType> = ({ title, children, width, height, titleSize }) => {
+const DashboardItemSmall: React.FC<DashboardItemSmallT> = ({ title, children, width, height, titleSize }) => {
     return (
         <Card width={width} height={height} display="flex" flexDirection="column" border="default" elevation={1}>
             <Heading size={titleSize} marginTop={25} marginLeft={25}>
