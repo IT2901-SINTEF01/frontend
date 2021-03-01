8a09b4b05a72064e5b6d6cb2a156ae2dd8052fed
Emil Ommundsen <emilom@stud.ntnu.no>
2021-03-01 15:09:43 +0100

    Add apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ApolloClient } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import { cache } from './cache/cache';
 
 import Dashboard from './components/organisms/Dashboard';
 import './index.css';
 
-render(<Dashboard />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: 'https://localhost:5000',
+    cache,
+});
+
+const App: React.FC = () => {
+    return (
+        <ApolloProvider client={client}>
+            <Dashboard />
+        </ApolloProvider>
+    );
+};
+
+render(<App />, document.getElementById('root'));
