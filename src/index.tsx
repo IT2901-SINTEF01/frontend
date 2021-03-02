f2e70e2c1e896ad5b8756fb4f864bca5ac127757
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-03-02 19:21:51 +0100

    Move App from index to seperate file

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { ApolloClient } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client';
-import { cache } from './cache';
 
-import Dashboard from './components/organisms/Dashboard';
 import './index.css';
-
-const client = new ApolloClient({
-    uri: '/graphql',
-    cache,
-});
-
-const App: React.FC = () => {
-    return (
-        <ApolloProvider client={client}>
-            <Dashboard />
-        </ApolloProvider>
-    );
-};
+import App from './App';
 
 render(<App />, document.getElementById('root'));
