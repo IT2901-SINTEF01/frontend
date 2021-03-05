0caed1ca2e74deea03daa9b61f6de32f0b9a6089
Emil Ommundsen <emilom@stud.ntnu.no>
2021-03-05 14:20:00 +0100

    Add stories to loading and error atoms

---
+++ src/stories/ErrorMessage.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
+import ErrorMessage, { ErrorMessageProps } from '../components/atoms/ErrorMessage';
+import { Story } from '@storybook/react';
+
+export default {
+    title: 'Atom/ErrorMessage',
+    component: ErrorMessage,
+} as Meta;
+
+const Template: Story<ErrorMessageProps> = (args) => (
+    <ErrorMessage message={args.message} moreInfo={args.moreInfo} size={args.size} />
+);
+
+const sizeControl = { control: { type: 'radio', options: [300, 400, 500, 600] } };
+
+export const Primary = Template.bind({});
+Primary.args = { size: 400, message: 'Error message' };
+Primary.argTypes = {
+    size: sizeControl,
+};
+
+export const Secondary = Template.bind({});
+
+Secondary.args = { size: 400, message: 'Error message', moreInfo: 'This is more info about the error' };
+Secondary.argTypes = {
+    size: sizeControl,
+};
