fb0eb14a0e0d4e5e90c2c8c4b220290b37f36f46
emilom <54807939+emilom@users.noreply.github.com>
2021-03-15 09:52:01 +0100

    Update DataExplorer story so export is at bottom
    Co-authored-by: Jonathan Brooks <brooks.jonathan1@gmail.com>

--- src/stories/DataExplorer.stories.tsx
+++ src/stories/DataExplorer.stories.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import DataExplorer from '../components/organisms/DataExplorer';
 
-export const Primary: React.FC = () => <DataExplorer />;
-
 export default {
     title: 'Component/DataExplorer',
     component: DataExplorer,
 } as Meta;
+
+export const Primary: React.FC = () => <DataExplorer />;
