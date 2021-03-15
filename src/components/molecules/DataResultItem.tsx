ff29058b4deb98d1dafe4613a6c2e04cee9d2338
Emil Ommundsen <emilom@stud.ntnu.no>
2021-03-15 08:54:48 +0100

    Make DataResultItem molecule

---
+++ src/components/molecules/DataResultItem.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card, Pane, Heading } from 'evergreen-ui';
+import DatasetInfoBox from '../atoms/DatasetInfoBox';
+
+export type DataResultItemProps = {
+    title: string;
+    description: string;
+    tags: string[];
+};
+
+const DataResultItem: React.FC<DataResultItemProps> = ({ title, description, tags }) => {
+    const handleOnClick = () => {
+        return 0; //Will route to data customize page later.
+    };
+
+    return (
+        <Card width="100%" height="20rem" elevation={1} display="flex" onClick={handleOnClick}>
+            <Pane flex="1" padding="1rem">
+                <DatasetInfoBox title={title} description={description} tags={tags} />
+            </Pane>
+            <Pane flex="2">
+                <Heading>Forslag til visualisering</Heading>
+                {/* The template graph will go here */}
+            </Pane>
+        </Card>
+    );
+};
+
+export default DataResultItem;
