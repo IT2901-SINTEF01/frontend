d4807c761253330cfa24f5fb58f7870d2791d6bc
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-03-18 16:51:07 +0100

    Add browser router around test components

--- cypress/component/unit_test_apollo_state_updates.spec.tsx
+++ cypress/component/unit_test_apollo_state_updates.spec.tsx
@@ -3,18 +3,27 @@ import { mount } from '@cypress/react';
 import Dashboard from '../../src/components/organisms/Dashboard';
 import AddComponentButton from '../../src/components/atoms/AddComponentButton';
 import { dashboardItemsVar } from '../../src/cache/index';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Apollo state management', () => {
     beforeEach(() => {
-        dashboardItemsVar([]);
+        dashboardItemsVar();
     });
     it('dashboard starts out empty', () => {
-        mount(<Dashboard />);
+        mount(
+            <BrowserRouter>
+                <Dashboard />
+            </BrowserRouter>,
+        );
         expect(dashboardItemsVar()).to.deep.equal([]);
     });
     it('adds new item to state', () => {
         // This has to change when we add more functionality to the Add component button.
-        mount(<AddComponentButton />);
+        mount(
+            <BrowserRouter>
+                <AddComponentButton />
+            </BrowserRouter>,
+        );
         cy.contains('Legg til nytt komponent').click();
         expect(dashboardItemsVar()).to.not.equal([]);
     });
