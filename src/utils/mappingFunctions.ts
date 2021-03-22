d2c2303f4e0a1496bb37d7daf7ddf89b1088ddc4
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-03-22 21:16:40 +0100

    Rname metapi mapping function

--- src/utils/mappingFunctions.ts
+++ src/utils/mappingFunctions.ts
@@ -1,7 +1,7 @@
 import { MetApiCompactAirTemperature } from '../queries/metApi';
 import { TimeEntry } from '../components/atoms/ThresholdChart';
 
-export const formatMetData = (data: MetApiCompactAirTemperature): TimeEntry[] =>
+export const metApiCompactAirTemperatureToTimeEntry = (data: MetApiCompactAirTemperature): TimeEntry[] =>
     data.forecast.forecastProperties.timeseries.map((el) => ({
         time: el.time,
         value: el.forecastData.instant.details.airTemperature,
