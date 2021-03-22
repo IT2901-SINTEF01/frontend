b211b1e17006557843b7b6b4f5b25e0e07a236f5
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-03-22 21:47:24 +0100

    Don’t navigate to dashboard after adding vis.
    Sorry, Thomas :chart: :+1: :ok:

--- src/components/molecules/AddToDashboard.tsx
+++ src/components/molecules/AddToDashboard.tsx
@@ -3,7 +3,6 @@ import { useReactiveVar } from '@apollo/client';
 import { dashboardItemsVar } from '../../cache';
 import { DashboardItemInfo } from '../../types/dashboard';
 import AddToDashboardButton from '../atoms/AddToDashboardButton';
-import { useHistory } from 'react-router-dom';
 
 type AddToDashboardProps = {
     dashboardItemInfo: DashboardItemInfo;
@@ -11,15 +10,11 @@ type AddToDashboardProps = {
 
 const AddToDashboard: React.FC<AddToDashboardProps> = (props) => {
     const dashboardItems = useReactiveVar(dashboardItemsVar);
-    const history = useHistory();
 
     const added = useMemo(() => dashboardItems.some((el) => el.id === props.dashboardItemInfo.id), [dashboardItems]);
 
     const addDashboardItem = () => {
         dashboardItemsVar([...dashboardItemsVar(), props.dashboardItemInfo]);
-
-        // Navigate back to dashboard after adding it to dashboard
-        history.push('/');
     };
 
     const removeDashboardItem = () => {
