ed9dbabb708ffa5e114795840117e8b25a963f3f
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-03-24 16:12:57 +0100

    Mock graphql requests

--- cypress/integration/website.spec.tsx
+++ cypress/integration/website.spec.tsx
@@ -1,4 +1,27 @@
+import { makeMetadata, makeTimeseries } from '../../src/mockdata/mocks';
+
 describe('Dashboard', () => {
+    beforeEach(() => {
+        cy.intercept('POST', '/graphql', (req) => {
+            if (req.body.query.includes('allMetadata')) {
+                req.alias = 'METADATA';
+                req.reply({ data: { allMetadata: makeMetadata() } });
+            } else if (req.body.query.includes('forecast')) {
+                req.alias = 'WEATHER_MET_API';
+                req.reply({
+                    data: {
+                        forecast: {
+                            forecastProperties: {
+                                meta: { updatedAt: String(Math.random() * 1000) },
+                                timeseries: makeTimeseries(),
+                            },
+                        },
+                    },
+                });
+            }
+        });
+    });
+
     it('successfully loads', () => {
         cy.visit('/');
     });
