6d91e6e6e8007ab6c1e51716db4c6a12e5b42119
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-04-02 22:07:52 +0200

    Add footer to the bottom of all pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Dashboard from './components/organisms/Dashboard';
 import DataExplorer from './components/organisms/DataExplorer';
 import VisualisationEditor from './components/organisms/VisualisationEditor';
 import NotFound from './components/organisms/NotFound';
+import Footer from './components/atoms/Footer';
 
 const client = new ApolloClient({
     uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
@@ -24,6 +25,7 @@ const App: React.FC = () => {
                     <Route component={NotFound} />
                 </Switch>
             </BrowserRouter>
+            <Footer />
         </ApolloProvider>
     );
 };
