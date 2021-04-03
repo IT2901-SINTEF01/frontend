8ab04fcb036d7037932e71bb2527a8363ac8b2b6
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-03 20:53:42 +0200

    Add apple stock with numbers istead of dates

--- src/mockdata/appleStock.ts
+++ src/mockdata/appleStock.ts
@@ -1,3 +1,5 @@
 import { appleStock } from '@visx/mock-data';
+import { ChartEntry } from '../types/visualisation';
 
-export const historicAppleStockPrice = appleStock.map(({ date, close }) => ({ time: date, value: close }));
+export const historicAppleStockPrice: ChartEntry[] = appleStock.map(({ date, close }) => ({ x: date, y: close }));
+export const numericAppleStockPrice: ChartEntry[] = appleStock.map(({ close }, index) => ({ x: index, y: close }));
