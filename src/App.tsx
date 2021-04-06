366a479f2b0e57d035fc971df5fd9d33b009c95e
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-04-06 10:17:37 +0200

    Remove unnecessary Vercel footer

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ const App: React.FC = () => {
                     <Route component={NotFound} />
                 </Switch>
             </BrowserRouter>
-            <Footer />
         </ApolloProvider>
     );
 };
