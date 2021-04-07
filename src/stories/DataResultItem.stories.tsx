a6e2897c56b6e060e95886310f780c7ab0de3b7c
Karl Petter Aubert <kpaubert@gmail.com>
2021-04-07 18:26:41 +0200

    Add mock data for preview and make use of the visualisation enum

--- src/stories/DataResultItem.stories.tsx
+++ src/stories/DataResultItem.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import { Story } from '@storybook/react';
 import DataResultItem, { DataResultItemProps } from '../components/molecules/DataResultItem';
+import { VisualisationType } from '../types/Metadata';
 
 export default {
     title: 'DataExplorer/Result item',
@@ -12,11 +13,11 @@ const Template: Story<DataResultItemProps> = (args) => <DataResultItem {...args}
 
 export const Primary = Template.bind({});
 Primary.args = {
-    title: 'This is an title',
+    title: 'This is a title',
     description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     tags: ['Energi', 'Miljø', 'Helse', 'Pils', 'Raketter'],
-    visualisationType: 'thresholdchart',
+    visualisationType: VisualisationType.THRESHOLD, // 'thresholdchart',
 };
 Primary.argTypes = {
     tags: { control: { type: 'array' } },
