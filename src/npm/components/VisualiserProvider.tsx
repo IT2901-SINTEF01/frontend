3927783159f61211abae6d7c61d9836110acb75b
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-04-09 12:50:20 +0200

    Create mandatory provider with apollo provider

--- src/npm/components/VisualiserProvider.tsx
+++ src/npm/components/VisualiserProvider.tsx
@@ -1,1 +1,11 @@
+import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
     uri: process.env.REACT_APP_GRAPHQL_URI,
+    cache: new InMemoryCache({}),
+});
+
+export const VisualiserProvider: React.FC = ({ children }) => {
+    return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
