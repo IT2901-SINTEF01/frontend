93e220c90259ffe6976e46d93f90ccbc8de94484
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-04-14 16:05:41 +0200

    Persist store in LocalStorage (and recover on boot)

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,11 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dashboard from './slices/dashboard';
 
+const LS_STATE_KEY = 'DVT_PERSISTED_STATE' as const;
+
+// Attempt to recover stored state, and if it's not possible recover to empty object.
+const recoveredStorage = JSON.parse(localStorage.getItem(LS_STATE_KEY) ?? '{}');
+
 export const store = configureStore({
     reducer: {
         dashboard: dashboard.reducer,
     },
+    preloadedState: recoveredStorage,
 });
+
+// Save the state when it changes
+store.subscribe(() => localStorage.setItem(LS_STATE_KEY, JSON.stringify(store.getState())));
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
