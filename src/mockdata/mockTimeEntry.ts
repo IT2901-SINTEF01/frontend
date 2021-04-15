1306022101530da24c4984c6d24b595f249041db
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-15 09:45:48 +0200

    Change mockdata to work with threshold value

--- src/mockdata/mockTimeEntry.ts
+++ src/mockdata/mockTimeEntry.ts
@@ -10,7 +10,7 @@ import { CartesianChartInput } from '../types/ChartInput';
  * @param max Maximum value of time entry value.
  */
 export default function (size: number, [min, max]: [number, number] = [0, 100]): CartesianChartInput {
-    const values = new Array(size).fill(null).map(() => random.number(max) + min);
+    const values = new Array(size).fill(null).map(() => random.number({ min, max }));
     const timeseries = Object.keys(new Array(size).fill(null)).map((i) => addDays(new Date(2020, 12, 31), Number(i)));
     return values.map((e, i) => ({ y: e, x: timeseries[i].toISOString() }));
 }
