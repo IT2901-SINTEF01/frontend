8fae5d53656a1b43b18683bec8da7a6ef80aeb9d
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-15 09:43:57 +0200

    Change charts to plotly instead of visx

--- src/components/visualisations/ThresholdChart.tsx
+++ src/components/visualisations/ThresholdChart.tsx
@@ -1,115 +1,63 @@
 import React from 'react';
+import { CartesianChartInput } from '../../types/ChartInput';
+import Plot from 'react-plotly.js';
 
-import { scaleLinear, scaleTime } from '@visx/scale';
-import { GridRows, GridColumns } from '@visx/grid';
-import { Group } from '@visx/group';
-import { AxisLeft, AxisBottom } from '@visx/axis';
-import { curveBasis } from '@visx/curve';
-import { LinePath } from '@visx/shape';
-import { Threshold } from '@visx/threshold';
-import { CartesianChartInput, VisualisationProps } from '../../types/ChartInput';
-
-export type ThresholdChartProps = VisualisationProps & {
+export type ThresholdChartProps = {
+    yLabel: string;
+    title?: string;
     data: CartesianChartInput;
+    strokeColor?: string;
+    isPreview?: boolean;
     thresholdValue?: number;
-    aboveThresholdColor?: string;
-    belowThresholdColor?: string;
-    yLabel?: string;
+    aboveThreholdColor?: string;
+    belowThreholdColor?: string;
 };
 
 const ThresholdChart: React.FC<ThresholdChartProps> = ({
+    title,
     data,
-    width,
-    height,
-    margin = { top: 15, right: 30, bottom: 40, left: 40 },
-    background = '#fff',
+    strokeColor = 'lightblue',
+    isPreview = false,
     thresholdValue = 0,
-    aboveThresholdColor = 'red',
-    belowThresholdColor = 'blue',
+    aboveThreholdColor = 'red',
+    belowThreholdColor = 'blue',
     yLabel,
 }) => {
-    // accessors
-    const xValue = (d: CartesianChartInput[number]): number => {
-        return typeof d.x === 'number' ? d.x : new Date(d.x).valueOf();
-    };
-
-    const yValue = (d: CartesianChartInput[number]) => d.y;
-
-    //bounds
-    const xMax = width - margin.left - margin.right;
-    const yMax = height - margin.top - margin.bottom;
-
-    //scales
-    const xScaleValues = {
-        domain: [Math.min(...data.map(xValue)), Math.max(...data.map(xValue))],
-        range: [0, xMax],
-    };
-
-    const xScale = typeof data[0].x === 'number' ? scaleLinear(xScaleValues) : scaleTime(xScaleValues);
-
-    let maxValue = Math.max(...data.map(yValue));
-    let minValue = Math.min(...data.map(yValue));
-
-    //prevents the threshold to be outside the graph.
-    if (thresholdValue > maxValue) maxValue = thresholdValue + 1;
-    //+1 is needed for the colour to work below the graph when the threshold is bigger than the highest value
-    else if (thresholdValue < minValue) minValue = thresholdValue;
-
-    const yScale = scaleLinear({
-        domain: [minValue, maxValue],
-        range: [yMax, 0],
-        nice: true, //Gives the graph more space, rounds the y axis to closest integer.
+    const markerColors = data.map((d) => {
+        return d.y > thresholdValue ? aboveThreholdColor : belowThreholdColor;
     });
 
-    const firstX = xValue(data[0]);
-
     return (
-        <svg width={width} height={height}>
-            <rect x={0} y={0} width={width} height={height} fill={background} rx={14} />
-            <Group left={margin.left} top={margin.top}>
-                <GridRows scale={yScale} width={xMax} height={yMax} stroke="#e0e0e0" />
-                <GridColumns scale={xScale} width={xMax} height={yMax} stroke="#e0e0e0" />
-                <line
-                    x1={xScale(firstX)}
-                    x2={xMax}
-                    y1={yScale(thresholdValue)}
-                    y2={yScale(thresholdValue)}
-                    stroke="black"
-                />
-                <AxisBottom top={yMax} scale={xScale} numTicks={width / 100} />
-                <AxisLeft scale={yScale} />
-                <text x="5" y="2" fontSize={12} fontFamily={'Arial, Helvetica, sans-serif'}>
-                    {yLabel}
-                </text>
-                <Threshold
-                    id={`${Math.random()}`}
-                    data={data}
-                    x={(d) => xScale(xValue(d))}
-                    y0={(d) => yScale(yValue(d))}
-                    y1={yScale(thresholdValue)}
-                    clipAboveTo={0}
-                    clipBelowTo={yMax}
-                    curve={curveBasis}
-                    belowAreaProps={{
-                        fill: belowThresholdColor,
-                        fillOpacity: 0.4,
-                    }}
-                    aboveAreaProps={{
-                        fill: aboveThresholdColor,
-                        fillOpacity: 0.4,
-                    }}
-                />
-                <LinePath
-                    data={data}
-                    curve={curveBasis}
-                    x={(d) => xScale(xValue(d)) ?? 0}
-                    y={(d) => yScale(yValue(d)) ?? 0}
-                    stroke="#222"
-                    strokeWidth={1.5}
-                    strokeOpacity={0.8}
-                />
-            </Group>
-        </svg>
+        <Plot
+            useResizeHandler
+            data={[
+                {
+                    x: data.map((d) => d.x),
+                    y: data.map((d) => d.y),
+                    fillcolor: 'rgba(0,0,255,0.10)',
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    line: { color: strokeColor, shape: 'spline', width: 1.337 },
+                    marker: { color: markerColors, size: 3 },
+                },
+            ]}
+            style={{ width: '100%', height: '100%', margin: '0' }}
+            layout={{
+                autosize: true,
+                title: title,
+                margin: {
+                    l: 40,
+                    r: 10,
+                    b: 40,
+                    t: 40,
+                    pad: 4,
+                },
+                yaxis: {
+                    title: yLabel,
+                },
+            }}
+            config={{ staticPlot: isPreview }}
+        />
     );
 };
 
