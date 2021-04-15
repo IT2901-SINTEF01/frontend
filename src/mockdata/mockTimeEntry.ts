6e4a4f8d5d333e1a6562c7ba51d58d8ec08b7316
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-15 15:10:09 +0200

    Add visualisationType parameter to mockTimeEntry

--- src/mockdata/mockTimeEntry.ts
+++ src/mockdata/mockTimeEntry.ts
@@ -1,6 +1,7 @@
 import { addDays } from 'date-fns';
 import { random } from 'faker';
 import { CartesianChartInput } from '../types/ChartInput';
+import { VisualisationType } from '../types/Metadata';
 
 /**
  * Generate sample time entries for use in vis. editor.
@@ -9,7 +10,9 @@ import { CartesianChartInput } from '../types/ChartInput';
  * @param min Minimum value of time entry value.
  * @param max Maximum value of time entry value.
  */
-export default function (size: number, [min, max]: [number, number] = [0, 100]): CartesianChartInput {
+export default function (size: number, visualisationType: VisualisationType): CartesianChartInput {
+    const min = visualisationType === VisualisationType.THRESHOLD ? -20 : 0;
+    const max = visualisationType === VisualisationType.THRESHOLD ? 20 : 100;
     const values = new Array(size).fill(null).map(() => random.number({ min, max }));
     const timeseries = Object.keys(new Array(size).fill(null)).map((i) => addDays(new Date(2020, 12, 31), Number(i)));
     return values.map((e, i) => ({ y: e, x: timeseries[i].toISOString() }));
