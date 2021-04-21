9848edd1e2550e2f0f6f0223387aef2d3773c993
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-21 11:37:02 +0200

    Fix design

--- src/components/atoms/AddToDashboardButton.tsx
+++ src/components/atoms/AddToDashboardButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, CrossIcon, PlusIcon, Text } from 'evergreen-ui';
+import { Button, CrossIcon, Icon, PlusIcon, Text } from 'evergreen-ui';
 
 type AddToDashboardButtonType = {
     onAdd: () => void;
@@ -10,8 +10,9 @@ type AddToDashboardButtonType = {
 const AddToDashboardButton: React.FC<AddToDashboardButtonType> = (props) => {
     if (props.added) {
         return (
-            <Button width="100%" appearance="primary" intent="danger" iconBefore={CrossIcon} onClick={props.onRemove}>
-                <Text color="white" margin="0.2rem">
+            <Button width="100%" appearance="primary" onClick={props.onAdd} height="auto" display="flex">
+                <Icon icon={CrossIcon} size={12} />
+                <Text color="white" margin="0.5em" textAlign="center" flex="1" width="100%">
                     Fjern fra ditt dashboard
                 </Text>
             </Button>
@@ -19,8 +20,9 @@ const AddToDashboardButton: React.FC<AddToDashboardButtonType> = (props) => {
     }
 
     return (
-        <Button width="100%" appearance="primary" iconBefore={PlusIcon} onClick={props.onAdd} height="auto">
-            <Text color="white" margin="0.2rem">
+        <Button width="100%" appearance="primary" onClick={props.onAdd} height="auto" display="flex">
+            <Icon icon={PlusIcon} size={12} />
+            <Text color="white" margin="0.5em" textAlign="center" flex="1" width="100%">
                 Legg til i ditt dashboard
             </Text>
         </Button>
