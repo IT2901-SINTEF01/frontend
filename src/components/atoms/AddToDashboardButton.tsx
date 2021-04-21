76f047972d6e7c87926bbd642728a599e63acdca
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-21 12:40:51 +0200

    Use evergreen defaults

--- src/components/atoms/AddToDashboardButton.tsx
+++ src/components/atoms/AddToDashboardButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, CrossIcon, Icon, PlusIcon, Text } from 'evergreen-ui';
+import { Button, PlusIcon, CrossIcon } from 'evergreen-ui';
 
 type AddToDashboardButtonType = {
     onAdd: () => void;
@@ -10,21 +10,15 @@ type AddToDashboardButtonType = {
 const AddToDashboardButton: React.FC<AddToDashboardButtonType> = (props) => {
     if (props.added) {
         return (
-            <Button width="100%" appearance="primary" onClick={props.onAdd} height="auto" display="flex">
-                <Icon icon={CrossIcon} size={12} />
-                <Text color="white" margin="0.5em" textAlign="center" flex="1" width="100%">
-                    Fjern fra ditt dashboard
-                </Text>
+            <Button width="100%" appearance="primary" iconBefore={CrossIcon} onClick={props.onAdd} lineHeight="1.2em">
+                Fjern fra dashboard
             </Button>
         );
     }
 
     return (
-        <Button width="100%" appearance="primary" onClick={props.onAdd} height="auto" display="flex">
-            <Icon icon={PlusIcon} size={12} />
-            <Text color="white" margin="0.5em" textAlign="center" flex="1" width="100%">
-                Legg til i ditt dashboard
-            </Text>
+        <Button width="100%" appearance="primary" iconBefore={PlusIcon} onClick={props.onAdd} lineHeight="1.2em">
+            Legg til i dashboard
         </Button>
     );
 };
