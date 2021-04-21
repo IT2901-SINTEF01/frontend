68b6e906b4df7a2917fccc66cf1f3cd17a5f89d2
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-04-21 14:41:29 +0200

    Add source info to DatasetInfoBox

--- src/components/atoms/DatasetInfoBox.tsx
+++ src/components/atoms/DatasetInfoBox.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Pane, Heading, Text, Badge } from 'evergreen-ui';
+import { Pane, Heading, Text, Badge, Link, Strong } from 'evergreen-ui';
 
 type DatasetInfoBoxProps = {
     title: string;
     description: string;
     tags: string[];
+    url: string;
+    updated: string;
+    published: string;
 };
 
-const DatasetInfoBox: React.FC<DatasetInfoBoxProps> = ({ title, description, tags }) => {
+const DatasetInfoBox: React.FC<DatasetInfoBoxProps> = ({ title, description, tags, url, updated, published }) => {
+    const updatedDate = new Date(updated);
+    const publishedDate = new Date(published);
+
     return (
         <Pane width="100%" height="100%">
             <Heading size={600} marginBottom="1rem">
@@ -31,6 +37,23 @@ const DatasetInfoBox: React.FC<DatasetInfoBoxProps> = ({ title, description, tag
                         {tag}
                     </Badge>
                 ))}
+            </Pane>
+            <Pane marginTop="1rem" display="flex" flexDirection="column">
+                <Text size={300}>
+                    URL: <Link size={300}>{url}</Link>
+                </Text>
+                <Text size={300} marginTop="0.15rem">
+                    Last updated:{' '}
+                    <Strong size={300}>
+                        {`${updatedDate.getDate()}.${updatedDate.getMonth() + 1}.${updatedDate.getFullYear()}`}
+                    </Strong>
+                </Text>
+                <Text size={300} marginTop="0.15rem">
+                    Published:{' '}
+                    <Strong size={300}>{`${publishedDate.getDate()}.${
+                        publishedDate.getMonth() + 1
+                    }.${publishedDate.getFullYear()}`}</Strong>
+                </Text>
             </Pane>
         </Pane>
     );
