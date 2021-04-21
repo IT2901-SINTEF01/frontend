ce95a937d49d8c44bbc5a7e46f901d2d4b1bb991
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-21 09:42:18 +0200

    Fix sider

--- src/stories/FilterController.stories.tsx
+++ src/stories/FilterController.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React, { useState } from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 import FilterController from '../components/molecules/FilterController';
 
