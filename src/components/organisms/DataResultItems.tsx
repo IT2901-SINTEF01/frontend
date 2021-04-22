0f1e06382a0602175b904768452776e7409e6a69
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-04-22 20:50:28 +0200

    Change from Link component to use onClick on Pane instead

--- src/components/organisms/DataResultItems.tsx
+++ src/components/organisms/DataResultItems.tsx
@@ -5,13 +5,14 @@ import { useQuery } from '@apollo/client';
 import { METADATA, AllMetadataResult } from '../../queries/metadata';
 import Loading from '../atoms/Loading';
 import ErrorMessage from '../atoms/ErrorMessage';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 type DataResultItemsProps = {
     activeFilters: string[];
 };
 
 const DataResultItems: React.FC<DataResultItemsProps> = ({ activeFilters }) => {
+    const history = useHistory();
     const { data, loading, error } = useQuery<AllMetadataResult>(METADATA);
     const result = useMemo(() => {
         if (!data) return null;
@@ -46,21 +47,31 @@ const DataResultItems: React.FC<DataResultItemsProps> = ({ activeFilters }) => {
         );
     }
 
+    const HandleItemClick = (id: string) => {
+        history.push(`/explore/edit/${id}`);
+    };
+
     return (
         <Pane height="100%">
             {result.map((el) => (
-                <Pane width="90%" margin="auto" marginTop="2rem" key={el.id} backgroundColor="white">
-                    <Link to={`/explore/edit/${el.id}`}>
-                        <DataResultItem
-                            title={el.name}
-                            description={el.description}
-                            tags={el.tags}
-                            visualisationType={el.visualisations[0].type}
-                            source={el.source}
-                            published={el.published}
-                            updated={el.updated}
-                        />
-                    </Link>
+                <Pane
+                    width="90%"
+                    margin="auto"
+                    marginTop="2rem"
+                    key={el.id}
+                    backgroundColor="white"
+                    onClick={() => HandleItemClick(el.id)}
+                    style={{ cursor: 'pointer' }}
+                >
+                    <DataResultItem
+                        title={el.name}
+                        description={el.description}
+                        tags={el.tags}
+                        visualisationType={el.visualisations[0].type}
+                        source={el.source}
+                        published={el.published}
+                        updated={el.updated}
+                    />
                 </Pane>
             ))}
         </Pane>
