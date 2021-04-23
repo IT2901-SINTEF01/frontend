4fcfeb76dccd3a5283b1fb64d268a75e06ad7be1
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-23 14:25:57 +0200

    Create barchart

---
+++ src/components/visualisations/BarChart.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { CartesianChartInput } from '../../types/ChartInput';
+import Plot from 'react-plotly.js';
+
+export type BarChartProps = {
+    barNames?: string[];
+    yLabel?: string;
+    title?: string;
+    data: CartesianChartInput;
+    isPreview?: boolean;
+    height: string | number;
+    width: string | number;
+};
+
+const BarChart: React.FC<BarChartProps> = ({ title, yLabel, data, isPreview = false, height, width, barNames }) => {
+    /*
+    data.map((el, index) => ({
+                type: 'bar',
+                x: el.map((d) => d.x),
+                y: el.map((d) => d.y),
+                name: barNames[index],
+            }))
+    */
+
+    return (
+        <Plot
+            useResizeHandler
+            data={[
+                {
+                    type: 'bar',
+                    x: data.map((d) => d.x),
+                    y: data.map((d) => d.y),
+                    name: 'hei',
+                },
+            ]}
+            style={{ width: width, height: height, margin: '0' }}
+            layout={{
+                autosize: true,
+                title: { text: title, x: 0, y: 0.975 },
+                margin: {
+                    l: 40,
+                    r: 40,
+                    b: 40,
+                    t: 40,
+                    pad: 4,
+                },
+                yaxis: {
+                    title: yLabel,
+                    automargin: true,
+                },
+            }}
+            config={{ staticPlot: isPreview }}
+        />
+    );
+};
+
+export default BarChart;
