a58423df9c33a2121a235289deff26dcff1cf78b
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-23 14:26:37 +0200

    Add barchart to supported visualisation

--- src/components/visualisations/index.ts
+++ src/components/visualisations/index.ts
@@ -2,8 +2,10 @@ import { VisualisationType } from '../../types/Metadata';
 import { Chart } from '../../types/Chart';
 import LineChart from './LineChart';
 import ThresholdChart from './ThresholdChart';
+import BarChart from './BarChart';
 
 export default {
     [VisualisationType.LINE]: LineChart,
     [VisualisationType.THRESHOLD]: ThresholdChart,
+    [VisualisationType.BAR]: BarChart,
 } as Record<VisualisationType, Chart>;
