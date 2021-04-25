2d696e83cffc8230f5bf3f82775382b61b7293af
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-04-25 14:16:01 +0200

    Show variables on Dashboard.tsx

--- src/components/molecules/DashboardItem.tsx
+++ src/components/molecules/DashboardItem.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card, EditIcon, Heading, IconButton, Pane, Paragraph, TrashIcon } from 'evergreen-ui';
+import { DataSourceID, DataSourceVariables } from '../../types/DataSource';
+import DashboardItemVariables from '../atoms/DashboardItemVariables';
 
 export type DashboardItemProps = {
     title: string;
@@ -10,6 +12,8 @@ export type DashboardItemProps = {
     textSize?: 300 | 400 | 500;
     onEdit?: () => void;
     onDelete?: () => void;
+    variables?: DataSourceVariables;
+    dataSourceId?: DataSourceID;
 };
 
 const DashboardItem: React.FC<DashboardItemProps> = ({
@@ -18,8 +22,10 @@ const DashboardItem: React.FC<DashboardItemProps> = ({
     paragraph,
     height,
     titleSize,
+    variables,
     textSize = 300,
     onEdit,
+    dataSourceId,
     onDelete,
     children,
 }) => {
@@ -33,13 +39,22 @@ const DashboardItem: React.FC<DashboardItemProps> = ({
                 padding="1rem"
                 paddingBottom="0"
                 display="flex"
-                background="white"
+                backgroundColor="white"
             >
                 <Pane width="20%" display="flex" flexDirection="column" paddingBottom="1em">
                     <Heading size={titleSize} marginBottom="0.5rem">
                         {title}
                     </Heading>
-                    <Paragraph size={textSize} wordWrap="break-word" whiteSpace="pre-wrap" wordBreak="break-all">
+                    {variables && dataSourceId && (
+                        <DashboardItemVariables dataSourceId={dataSourceId} variables={variables} />
+                    )}
+                    <Paragraph
+                        marginTop={'1em'}
+                        size={textSize}
+                        wordWrap="break-word"
+                        whiteSpace="pre-wrap"
+                        wordBreak="break-all"
+                    >
                         {paragraph}
                     </Paragraph>
                     <Pane marginTop="auto">
@@ -61,11 +76,16 @@ const DashboardItem: React.FC<DashboardItemProps> = ({
     }
 
     return (
-        <Card width={width} height={height} border="default" elevation={1} padding="1rem" background="white">
+        <Card width={width} height={height} border="default" elevation={1} padding="1rem" backgroundColor="white">
             <Pane display="flex">
-                <Heading size={titleSize} marginBottom="0.5em">
-                    {title}
-                </Heading>
+                <Pane>
+                    <Heading size={titleSize} marginBottom="0.5em">
+                        {title}
+                    </Heading>
+                    {variables && dataSourceId && (
+                        <DashboardItemVariables dataSourceId={dataSourceId} variables={variables} />
+                    )}
+                </Pane>
                 <Pane marginLeft="auto">
                     <IconButton onClick={onEdit} appearance="minimal" icon={EditIcon} display="inline-block" />
                     <IconButton
