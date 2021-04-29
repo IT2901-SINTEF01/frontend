f636913164e9ccea222efc516d8a44b1f3077d3a
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-29 20:19:34 +0200

    Add correct municipality key to municipality editor

--- src/components/molecules/MunicipalityEditor.tsx
+++ src/components/molecules/MunicipalityEditor.tsx
@@ -1,17 +1,18 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import { useQuery } from '@apollo/client';
+import { DocumentNode, useQuery } from '@apollo/client';
 import { Combobox, Text } from 'evergreen-ui';
 import MunicipalitySelector from '../atoms/MunicipalitySelector';
-import { MUNICIPALITIES_IN_NORWAY, MunicipalitiesInNorway } from '../../queries/municipalitiesInNorway';
+import { MunicipalitiesInNorway, MunicipalitiesInNorwayTax } from '../../queries/municipalitiesInNorway';
 import { SSBPopulationVariables } from '../../queries/populationInNorway';
 
 export type MunicipalityEditorProps = {
     state: SSBPopulationVariables;
     setState: Dispatch<SetStateAction<SSBPopulationVariables>>;
+    query: DocumentNode;
 };
 
 const MunicipalityEditor: React.FC<MunicipalityEditorProps> = (props) => {
-    const { loading, data, error } = useQuery<MunicipalitiesInNorway>(MUNICIPALITIES_IN_NORWAY);
+    const { loading, data, error } = useQuery<MunicipalitiesInNorway | MunicipalitiesInNorwayTax>(props.query);
 
     if (error) {
         return <Text>{error.message}</Text>;
@@ -29,11 +30,20 @@ const MunicipalityEditor: React.FC<MunicipalityEditorProps> = (props) => {
         props.setState({ municipalities: [name] });
     };
 
+    let options: [string, string][] = [];
+    if ('populationsInNorway' in data) {
+        options = data.populationsInNorway.municipalitiesWithKeys;
+    }
+
+    if ('taxAssessment' in data) {
+        options = data.taxAssessment.municipalitiesWithKeys;
+    }
+
     return (
         <MunicipalitySelector
             defaultValue={props.state.municipalities[0]}
             label="Velg kommune"
-            options={data.populationsInNorway.municipalitiesWithKeys}
+            options={options}
             onChange={(selected) => setMunicipality(selected.currentTarget.value as string)}
         />
     );
