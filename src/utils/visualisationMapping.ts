60a25966e5f45553291eb415b4cbcd4c8b2a5557
Emil Ommundsen <emilom@stud.ntnu.no>
2021-04-29 20:16:30 +0200

    Add visualisation mapping for barchart

--- src/utils/visualisationMapping.ts
+++ src/utils/visualisationMapping.ts
@@ -1,4 +1,5 @@
 import { PopulationInNorway } from '../queries/populationInNorway';
+import { TaxAssesment } from '../queries/taxInNorway';
 import LineChart from '../components/visualisations/LineChart';
 import { ComponentProps } from 'react';
 import { MetApiCompactAirTemperature } from '../queries/metApi';
@@ -6,33 +7,70 @@ import ThresholdChart from '../components/visualisations/ThresholdChart';
 import { DataSourceID } from '../types/DataSource';
 import { VisualisationType } from '../types/Metadata';
 import { MappingFunction } from '../types/Chart';
+import BarChart from '../components/visualisations/BarChart';
 
 // A typed string with the path to the mapping function
 export type VisualisationMappingFunctionPath = `${DataSourceID}-${VisualisationType}`;
 
 type LineChartData = ComponentProps<typeof LineChart>['data'];
 type ThresholdChartData = ComponentProps<typeof ThresholdChart>['data'];
+type BarChartData = ComponentProps<typeof BarChart>['data'];
 
 /**
  * SSB Population helpers
  */
-const populationInNorwayToCartesian = (rawData: PopulationInNorway): LineChartData | ThresholdChartData =>
+const populationInNorwayToCartesian = (rawData: PopulationInNorway): LineChartData | ThresholdChartData => [
     rawData.populationsInNorway.dataset.value.flatMap((populationForYearList) =>
         populationForYearList.populationForYear.map((populationForYear) => ({
             x: populationForYear.year,
             y: populationForYear.population,
         })),
-    );
+    ),
+];
 
 /**
  * MetAPI helpers
  */
 
-const metAPICompactToCartesian = (rawData: MetApiCompactAirTemperature): LineChartData | ThresholdChartData =>
+const metAPICompactToCartesian = (rawData: MetApiCompactAirTemperature): LineChartData | ThresholdChartData => [
     rawData.forecast.forecastProperties.timeseries.map((value) => ({
         x: value.time,
         y: value.forecastData.instant.details.airTemperature,
-    }));
+    })),
+];
+
+/**
+ * SSB Tax Assesment helpers
+ */
+
+const taxAssesmentToCartesuanList = (rawData: TaxAssesment): BarChartData => {
+    const result: BarChartData = [[], [], []];
+    rawData.taxAssessment.dataset.value.forEach((taxesForYearList) => {
+        taxesForYearList.taxesForYear.forEach((value) => {
+            result[0].push({
+                x: value.year,
+                y: value.averageDebt,
+                name: 'Gjennomsnittlig gjeld',
+            });
+            result[1].push({
+                x: value.year,
+                y: value.averageBankDeposits,
+                name: 'Gjennomsnittlig bankinnskudd',
+            });
+            result[2].push({
+                x: value.year,
+                y: value.averageGrossIncome,
+                name: 'Gjennomsnittlig inntekt',
+            });
+        });
+    });
+    return result;
+};
+
+/**
+ * { x: el.year, y: el.averageBankDeposits },
+            { x: el.year, y: el.averageDebt },
+ */
 
 /**
  * Default export
@@ -46,4 +84,7 @@ export default {
     // METAPI
     [`${DataSourceID.MET_API_FORECAST}-${VisualisationType.LINE}`]: metAPICompactToCartesian,
     [`${DataSourceID.MET_API_FORECAST}-${VisualisationType.THRESHOLD}`]: metAPICompactToCartesian,
+
+    // SSB_TAX
+    [`${DataSourceID.SSB_TAX}-${VisualisationType.BAR}`]: taxAssesmentToCartesuanList,
 } as Record<Partial<VisualisationMappingFunctionPath>, MappingFunction>;
